7823eb4ceebf82f3c45dd04d83da1abd2dfc1942
TranvandatWhiteX <tranvandatdh012@gmail.com>
2023-11-19 21:22:36 +0700

    update and delete function, delete has some error

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const multer = require('multer')
 const User = require('../models/users')
+const fs = require('fs')
 //Image Upload
 var storage = multer.diskStorage({
     destination: function(req, file, cb) {
@@ -48,5 +49,69 @@ router.get('/', (req, res) => {
 router.get('/add-user', (req, res) => {
     res.render("./layout/add_users", {title: "Add Users"})
 })
-
+// Edit user route
+router.get('/edit-user/:id', (req, res)=> {
+    let id = req.params.id
+    User.findById(id).exec().then((user)=> {
+        res.render("./layout/edit_users", {
+            title: "Edit User",
+            user: user
+        })
+    }).catch((err) => {
+        res.redirect("/")
+    })
+})
+// Update user route
+router.post("/update-user/:id", upload, (req, res) => {
+    let id = req.params.id 
+    let new_image = ""
+    if (req.file) {
+        new_image = req.file.fieldname
+        try {
+            fs.unlinkSync('./uploads/'+req.body.old_image)
+        } catch (error) {
+            console.log(error)
+        }
+    } else {
+        new_image = req.body.old_image
+    }
+    User.findByIdAndUpdate(id, {
+        name: req.body.name,
+        email: req.body.email,
+        phone: req.body.phone,
+        image: new_image
+    }).exec().then(() => {
+        req.session.message = {
+            type: "success",
+            message: "User updated successfully!"
+        }
+        res.redirect('/')
+    }).catch((err)=> {res.json({
+        type: "danger",
+        message: err.message
+    })})
+})
+//Delete user route
+router.get('/delete-user/:id', (req,res) => {
+    let id = req.params.id
+    User.findOneAndDelete(id).exec().then(
+        (result) => {
+            if (result.image != "") {
+                try {
+                    fs.unlinkSync('./uploads/'+ result.image)
+                } catch (error) {
+                    console.log(error)
+                }
+            }
+            req.session.message = {
+                type: "info",
+                message: "User deleted successfully!" 
+            }
+            res.redirect('/')
+        }
+    ).catch((err)=> {res.json({
+        type: "danger",
+        message: err.message
+    })})
+})
 module.exports = router
